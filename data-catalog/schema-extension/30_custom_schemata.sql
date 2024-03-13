c980042e8d47fa9492b913ed6e37b54fee35fa87
Kostas Patroumpas <kpatro@sdl-1.lxd>
2024-03-13 12:34:41 +0000

    Minor update to custom SQL schema for storing execution metadata

--- data-catalog/schema-extension/30_custom_schemata.sql
+++ data-catalog/schema-extension/30_custom_schemata.sql
@@ -16,12 +16,12 @@ CREATE TYPE state_enum AS ENUM ('created', 'restarting', 'running', 'removing', 
 -- DROP TABLE klms.workflow_execution;
 
 CREATE TABLE klms.workflow_execution
-( run_uuid varchar(64) NOT NULL,
+( workflow_uuid varchar(64) NOT NULL,
   "state" state_enum NOT NULL, 
   start_date timestamp,
   end_date timestamp,
 --  package_id text,
-  PRIMARY KEY (run_uuid)
+  PRIMARY KEY (workflow_uuid)
 --,  CONSTRAINT fk_workflow_id FOREIGN KEY(package_id) REFERENCES public.package(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
@@ -29,13 +29,12 @@ CREATE TABLE klms.workflow_execution
 -- DROP TABLE klms.workflow_tag;
 
 CREATE TABLE klms.workflow_tag
-( run_uuid varchar(64) NOT NULL,
+( workflow_uuid varchar(64) NOT NULL,
   "key" text NOT NULL, 
   "value" text,
-  PRIMARY KEY (run_uuid, "key"),
-  CONSTRAINT fk_workflow_tag_uuid FOREIGN KEY(run_uuid) REFERENCES klms.workflow_execution(run_uuid) ON UPDATE CASCADE ON DELETE CASCADE
+  PRIMARY KEY (workflow_uuid, "key"),
+  CONSTRAINT fk_workflow_tag_uuid FOREIGN KEY(workflow_uuid) REFERENCES klms.workflow_execution(workflow_uuid) ON UPDATE CASCADE ON DELETE CASCADE
 );
-
 
 ---------------------------------------------
 --           TASK EXECUTIONS
@@ -44,17 +43,16 @@ CREATE TABLE klms.workflow_tag
 -- DROP TABLE klms.task_execution;
 
 CREATE TABLE klms.task_execution
-( run_uuid varchar(64) NOT NULL,
+( task_uuid varchar(64) NOT NULL,
   workflow_uuid varchar(64) NOT NULL,
   "state" state_enum NOT NULL, 
   start_date timestamp,
   end_date timestamp,
   next_task_uuid varchar(64),
-  PRIMARY KEY (run_uuid),
-  CONSTRAINT fk_workflow_uuid FOREIGN KEY(workflow_uuid) REFERENCES klms.workflow_execution(run_uuid) ON UPDATE CASCADE ON DELETE CASCADE,
-  CONSTRAINT fk_next_task_uuid FOREIGN KEY(next_task_uuid) REFERENCES klms.task_execution(run_uuid) ON UPDATE CASCADE ON DELETE SET NULL
+  PRIMARY KEY (task_uuid),
+  CONSTRAINT fk_workflow_uuid FOREIGN KEY(workflow_uuid) REFERENCES klms.workflow_execution(workflow_uuid) ON UPDATE CASCADE ON DELETE CASCADE,
+  CONSTRAINT fk_next_task_uuid FOREIGN KEY(next_task_uuid) REFERENCES klms.task_execution(task_uuid) ON UPDATE CASCADE ON DELETE SET NULL
 );
-
 
 
 -- DROP TABLE klms.task_tag;
@@ -64,9 +62,8 @@ CREATE TABLE klms.task_tag
   "key" text NOT NULL, 
   "value" text,
   PRIMARY KEY (task_uuid, "key"),
-  CONSTRAINT fk_task_tag_uuid FOREIGN KEY(task_uuid) REFERENCES klms.task_execution(run_uuid) ON UPDATE CASCADE ON DELETE CASCADE
+  CONSTRAINT fk_task_tag_uuid FOREIGN KEY(task_uuid) REFERENCES klms.task_execution(task_uuid) ON UPDATE CASCADE ON DELETE CASCADE
 );
-
 
 -- DROP TABLE klms.metrics;
 
@@ -76,9 +73,8 @@ CREATE TABLE klms.metrics
   "value" text,
   issued timestamp,
   PRIMARY KEY (task_uuid, "key", issued),
-  CONSTRAINT fk_task_metrics_uuid FOREIGN KEY(task_uuid) REFERENCES klms.task_execution(run_uuid) ON UPDATE CASCADE ON DELETE CASCADE
+  CONSTRAINT fk_task_metrics_uuid FOREIGN KEY(task_uuid) REFERENCES klms.task_execution(task_uuid) ON UPDATE CASCADE ON DELETE CASCADE
 );
-
 
 
 -- DROP TABLE klms.parameters;
@@ -88,7 +84,7 @@ CREATE TABLE klms.parameters
   "key" text NOT NULL, 
   "value" text,
   PRIMARY KEY (task_uuid, "key"),
-  CONSTRAINT fk_task_parameters_uuid FOREIGN KEY(task_uuid) REFERENCES klms.task_execution(run_uuid) ON UPDATE CASCADE ON DELETE CASCADE
+  CONSTRAINT fk_task_parameters_uuid FOREIGN KEY(task_uuid) REFERENCES klms.task_execution(task_uuid) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 
@@ -98,7 +94,7 @@ CREATE TABLE klms.task_input
 ( task_uuid varchar(64) NOT NULL,
   dataset_id text NOT NULL, 
   PRIMARY KEY (task_uuid, dataset_id),
-  CONSTRAINT fk_task_input_uuid FOREIGN KEY(task_uuid) REFERENCES klms.task_execution(run_uuid) ON UPDATE CASCADE ON DELETE CASCADE,
+  CONSTRAINT fk_task_input_uuid FOREIGN KEY(task_uuid) REFERENCES klms.task_execution(task_uuid) ON UPDATE CASCADE ON DELETE CASCADE,
   CONSTRAINT fk_task_input_dataset FOREIGN KEY(dataset_id) REFERENCES public.resource(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
@@ -109,10 +105,9 @@ CREATE TABLE klms.task_output
 ( task_uuid varchar(64) NOT NULL,
   dataset_id text NOT NULL, 
   PRIMARY KEY (task_uuid, dataset_id),
-  CONSTRAINT fk_task_output_uuid FOREIGN KEY(task_uuid) REFERENCES klms.task_execution(run_uuid) ON UPDATE CASCADE ON DELETE CASCADE,
+  CONSTRAINT fk_task_output_uuid FOREIGN KEY(task_uuid) REFERENCES klms.task_execution(task_uuid) ON UPDATE CASCADE ON DELETE CASCADE,
   CONSTRAINT fk_task_output_dataset FOREIGN KEY(dataset_id) REFERENCES public.resource(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
-
 
 
 -- ***************************************
