6af6b8f38c1c6e88dd11eb385c531480311dbfce
Vasilis Samoladas <vsam@softnet.tuc.gr>
2024-04-01 15:18:03 +0300

    New version of container image creation

--- data-catalog/postgis-k8s/docker-entrypoint-initdb.d/30_setup_test_databases.sh
+++ data-catalog/postgis-k8s/docker-entrypoint-initdb.d/30_setup_test_databases.sh
@@ -5,3 +5,11 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     CREATE DATABASE ckan_test OWNER "$CKAN_DB_USER" ENCODING 'utf-8';
     CREATE DATABASE datastore_test OWNER "$CKAN_DB_USER" ENCODING 'utf-8';
 EOSQL
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" ckan_test <<-EOSQL
+    CREATE EXTENSION postgis;
+EOSQL
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" datastore_test <<-EOSQL
+    CREATE EXTENSION postgis;
+EOSQL
