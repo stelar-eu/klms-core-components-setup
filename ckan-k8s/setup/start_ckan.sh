53815e56218606046ea467a76ba00b79931f380f
Dimitris Petrou <petroudimitris@outlook.com>
2025-04-03 22:18:50 +0300

    CKAN Session secret and JWT keys are fetched from the environment via the appropriate K8s secrets

--- ckan-k8s/setup/start_ckan.sh
+++ ckan-k8s/setup/start_ckan.sh
@@ -12,11 +12,10 @@ if [ "$1" = "setup" ]; then
 
     if grep -E "beaker.session.secret ?= ?$" ckan.ini; then
         echo "[CKAN_SETUP] Setting beaker.session.secret in ini file"
-        ckan config-tool $CKAN_INI "beaker.session.secret=$(python3 -c 'import secrets; print(secrets.token_urlsafe())')"
+        ckan config-tool $CKAN_INI "beaker.session.secret=${CKAN___BEAKER__SESSION__SECRET}"
         ckan config-tool $CKAN_INI "WTF_CSRF_SECRET_KEY=$(python3 -c 'import secrets; print(secrets.token_urlsafe())')"
-        JWT_SECRET=$(python3 -c 'import secrets; print("string:" + secrets.token_urlsafe())')
-        ckan config-tool $CKAN_INI "api_token.jwt.encode.secret=${JWT_SECRET}"
-        ckan config-tool $CKAN_INI "api_token.jwt.decode.secret=${JWT_SECRET}"
+        ckan config-tool $CKAN_INI "api_token.jwt.encode.secret=${CKAN___API_TOKEN__JWT__ENCODE__SECRET}"
+        ckan config-tool $CKAN_INI "api_token.jwt.decode.secret=${CKAN___API_TOKEN__JWT__DECODE__SECRET}"
     fi
     echo " "
 
